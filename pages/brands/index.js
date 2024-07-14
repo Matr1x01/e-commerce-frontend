a0fef515251282a61bcb2f96948f655a8175f8f5
Mozharul Haq <mozharul.haq@appnap.io>
2024-07-14 18:45:43 +0600

    small fixes

--- pages/brands/index.js
+++ pages/brands/index.js
@@ -1,11 +1,11 @@
 'use client'
 import BandCard from "@/components/BrandCard"; // Assuming you have a BandCard component
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import {useState} from 'react';
+import {useRouter} from 'next/router';
 import Pagination from '@/components/Pagination';
 import {getBrands} from "@/api/productRequests";
 
-const Bands = ({ bandsData }) => {
+const Bands = ({bandsData}) => {
     const router = useRouter();
     let meta = bandsData.data.meta;
     const [currentPage, setCurrentPage] = useState(parseInt(meta?.current_page) || 1);
@@ -29,15 +29,15 @@ const Bands = ({ bandsData }) => {
                     </div>
                 ))}
             </div>
-            <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+            <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange}/>
         </div>
     );
 };
 
-export const getServerSideProps = async ({ query }) => {
-    const perPage = query.per_page || 5;
+export const getServerSideProps = async ({query}) => {
+    const perPage = query.per_page || 10;
     const page = query.page || 1;
-    const response = await getBrands({page: page, per_page: perPage});
+    const response = await getBrands({query: {page: page, per_page: perPage}});
 
     if (response.error) {
         return {
